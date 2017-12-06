82c44c59d7acdebd9b21dbe033bd76dac5eb2ba7
gbri140 <glb3@rice.edu>
2017-12-05 19:24:44 -0600

    Update HW6.m

--- HW6.m
+++ HW6.m
@@ -1,4 +1,14 @@
 %HW6
+%GB comments
+1a 100
+1b 100
+1c 100
+1d 100
+1e 100
+2a 100
+2b 100
+2c 100
+Overall: 100
 
 % Problem 1. Curve fitting. 
 % Part 1. Take 10 x values on the interval 0 < x <= 10 and then create y
